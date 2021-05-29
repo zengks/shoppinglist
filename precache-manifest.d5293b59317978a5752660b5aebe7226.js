a2106627256635457c9e2cf1e8328add861931fc
zengks <stevevitasks@hotmail.com>
2021-05-28 17:13:23 -0700

    Updates

---
+++ precache-manifest.d5293b59317978a5752660b5aebe7226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df5eac0fb3120c3e6c86c29e9d6cf9b",
+    "url": "/shoppinglist/index.html"
+  },
+  {
+    "revision": "2b1e5f2bde1c3048dd91",
+    "url": "/shoppinglist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "51785805377f84842a6d",
+    "url": "/shoppinglist/static/js/2.142636ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shoppinglist/static/js/2.142636ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1e5f2bde1c3048dd91",
+    "url": "/shoppinglist/static/js/main.46c312ab.chunk.js"
+  },
+  {
+    "revision": "de886bf9695d3a56d14f",
+    "url": "/shoppinglist/static/js/runtime-main.4448b595.js"
+  }
+]);
